800150f98ac8496af615551707cb1e109627ba48
kakeyakeito <muryoku0es@icloud.com>
2019-07-17 05:20:56 +0000

    Fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :books
-  get '/' => 'books#start'
+  root to: 'books#start'
   # For details on the DSget '/' => 'books#start'L available within this file, see http://guides.rubyonrails.org/routing.html
 end
